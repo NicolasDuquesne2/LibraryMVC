223967207b7963f44d3c7717dc9955ca26d6c756
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2024-01-23 17:32:11 +0100

    Revert "Revert "Config : staring project environment""
    This reverts commit a1c686d4bd6a7b1dbe1a14aa38a86e3def187763.

---
+++ LibraryMVC.Core/Entities/Adresse.cs
@@ -0,0 +1,21 @@
+﻿using LibraryMVC.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMVC.Entities
+{
+    public class Adresse:Entity
+    {
+        public string Appartement { get; set; } = null!;
+        public string Rue { get; set; } = null!;
+        public string Ville { get; set; } = null!;
+        public string CodePostal { get; set; } = null!;
+        public string Pays { get; set; } = null!;
+
+        public HashSet<Lecteur> Lecteurs { get; } = new HashSet<Lecteur>();
+    }
+}
